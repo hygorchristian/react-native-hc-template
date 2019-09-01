5b4e7950129062f877343d21237acf085dcacce7
Hygor Christian Dias <hygor.christian@gmail.com>
2019-08-31 21:33:43 -0300

    atualizando para styled-components

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,59 +1,26 @@
 import React from 'react';
 
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+  Container, Status, Logo, Welcome, Instruction
+} from './styles';
 
 const Main = () => (
-  <ImageBackground
-    source={{
+  <Container
+    sou
       uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
     }}
-    style={styles.container}
-    resizeMode="cover"
   >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
+    <Status barStyle="light-content" backgroundColor="#7159c1" />
+    <Logo
       source={{
         uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
       }}
-      style={styles.logo}
-      resizeMode="contain"
     />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
+    <Welcome style={styles.welcome}>Bem-vindo ao Template Básico!</Welcome>
+    <Instruction>Essa é a tela principal da sua aplicação =)</Instruction>
+    <Instruction>Você pode editar a tela no arquivo:</Instruction>
+    <Instruction bold>src/pages/Main/index.js</Instruction>
+  </Container>
 );
 
 export default Main;
---
+++ src/screens/Main/styles.js
@@ -0,0 +1,49 @@
+import { Dimensions } from 'react-native';
+import styled from 'styled-components';
+
+
+const LOGO_HEIGHT = Dimensions.get('window').height * 0.11;
+const LOGO_WIDTH = LOGO_HEIGHT * (1950 / 662);
+
+export const Container = styled.ImageBackground.attrs({
+  resizeMode: 'contain'
+})`
+  align-items: center;
+  flex: 1;
+  padding-horizontal: 20px;
+`;
+
+export const Status = styled.StatusBar.attrs({
+  barStyle: 'light-content',
+  backgroundColor: '#429dc1'
+})``;
+
+export const Logo = styled.Image.attrs({
+  resizeMode: 'contain'
+})`
+  height: ${LOGO_HEIGHT}px;
+  width: ${LOGO_WIDTH}px;
+  margin-vertical: ${LOGO_HEIGHT}px;
+`;
+
+export const Welcome = styled.Text`
+  color: #fff;
+  font-size: 22;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const Instruction = styled.Text`
+  color: #ddd;
+  font-size: 14px;
+  margin-top: ${({ bold }) => bold ? '5px' : '20px'};
+  text-align: center;
+  ${({ bold }) => bold && css`
+    font-weight: bold;
+  `}
+`;
+
+
+
+
+
