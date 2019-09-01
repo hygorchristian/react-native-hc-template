b577c89cd77fd54f69accd21bf1ee70496f13736
Hygor Christian Dias <hygor.christian@gmail.com>
2019-08-31 21:03:53 -0300

    cri

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,8 +1,19 @@
+import url from 'url';
+import { NativeModules } from 'react-native';
+
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux as reduxPlugin } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
+
+const { hostname } = url.parse(NativeModules.SourceCode.scriptURL);
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
-    .useReactNative()
+  const tron = Reactotron.configure({ host: hostname, name: 'React Native Demo' })
+    .useReactNative({
+      asyncStorage: false,
+    })
+    .use(sagaPlugin())
+    .use(reduxPlugin())
     .connect();
 
   tron.clear();
