71638aa1c5f7a7be0d884463d4e33adb5628ad35
Hygor <hygor@x-apps.com.br>
2019-11-23 08:48:50 -0300

    melhorando as rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,39 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from '~/screens/Main';
 
+const Stack = createStackNavigator(
+  {
+    // Screen: {
+    //   screen: Screen,
+    //   navigationOptions: {
+    //     header: null,
+    //     gesturesEnabled: false,
+    //   },
+    // },
+  },
+  {
+    // mode: 'modal',
+    // headerMode: 'none',
+  }
+);
+
 const Routes = createSwitchNavigator({
   Main: {
-    screen: Main
-  }
+    screen: Main,
+    navigationOptions: {
+      header: null,
+      gesturesEnabled: false,
+    },
+  },
+  // Stack: {
+  //   screen: Stack,
+  //   navigationOptions: {
+  //     header: null,
+  //     gesturesEnabled: false,
+  //   },
+  // },
 });
 
 export default createAppContainer(Routes);
